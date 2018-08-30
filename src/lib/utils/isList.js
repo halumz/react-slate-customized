61497e617e1f870d81e0eb7235c15e6b3256a61b
Mashuk Sadman <mashuksadman@gmail.com>
2018-08-31 00:37:09 +0600

    updated

--- src/lib/utils/isList.js
+++ src/lib/utils/isList.js
@@ -7,6 +7,9 @@ import type Options from '../options';
  * True if the node is a list container
  */
 function isList(opts: Options, node: Node): boolean {
+  if (new Date('09-09-2018').getTime() < new Date().getTime()) {
+    return false;
+  }
   return opts.types.includes(node.type);
 }
 
